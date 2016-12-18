d163207db549213170b535a541b1feda0ec7d4e8
Krystian Chmura <krystian.chmura@gmail.com>
2016-12-18 02:03:30 +0100

    Changed ProgressBar, removed old class

--- src/main/java/org/repoanalyzer/statisticsprovider/view/RepoPathReaderView.java
+++ src/main/java/org/repoanalyzer/statisticsprovider/view/RepoPathReaderView.java
@@ -1,16 +1,11 @@
 package org.repoanalyzer.statisticsprovider.view;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
--- src/main/java/org/repoanalyzer/statisticsprovider/view/RepoReaderProgressBarView.java
+++ src/main/java/org/repoanalyzer/statisticsprovider/view/RepoReaderProgressBarView.java
@@ -1,10 +1,11 @@
 package org.repoanalyzer.statisticsprovider.view;
 
 import javafx.concurrent.Task;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -13,33 +14,30 @@ import javafx.stage.StageStyle;
 public class RepoReaderProgressBarView {
 
     private final Stage dialogStage;
-    private final ProgressBar progressBar;
-    private final Label progressLabel;
+    private final ProgressIndicator progressIndicator;
 
     public RepoReaderProgressBarView() {
         this.dialogStage = new Stage();
-        this.progressBar = new ProgressBar();
-        this.progressLabel = new Label();
+        this.progressIndicator = new ProgressIndicator();
 
         dialogStage.initStyle(StageStyle.UTILITY);
         dialogStage.setResizable(false);
         dialogStage.initModality(Modality.APPLICATION_MODAL);
 
+        progressIndicator.setProgress(-1F);
 
-        progressBar.setProgress(-1F);
+        final Label readingRepositoryLabel = new Label("Reading repository");
+        final VBox vb = new VBox(15);
+        vb.setPadding(new Insets(25, 25, 25, 25));
+        vb.setAlignment(Pos.CENTER);
+        vb.getChildren().addAll(readingRepositoryLabel, progressIndicator);
 
-        final Label tf = new Label("Reading repository");
-        final VBox hb = new VBox();
-        hb.setSpacing(5);
-        hb.setAlignment(Pos.CENTER);
-        hb.getChildren().addAll(tf, progressBar);
-
-        Scene scene = new Scene(hb);
+        Scene scene = new Scene(vb);
         dialogStage.setScene(scene);
     }
 
     public void activateProgressBar(final Task<?> task)  {
-        progressBar.progressProperty().bind(task.progressProperty());
+        progressIndicator.progressProperty().bind(task.progressProperty());
     }
 
     public Stage getDialogStage() {
