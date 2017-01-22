aa9d829ab353b283c491c6d20c20bedcba256a97
Pawel Palimaka <Pawel.Palimaka@sabre.com>
2017-01-23 00:55:56 +0100

    Added primitive GitRepoReader:getCommits() test

--- src/test/java/org/repoanalyzer/reporeader/git/GitRepoReaderTest.java
+++ src/test/java/org/repoanalyzer/reporeader/git/GitRepoReaderTest.java
@@ -4,16 +4,32 @@ import org.junit.Test;
 import org.repoanalyzer.reporeader.Progress;
 import org.repoanalyzer.reporeader.author.Author;
 import org.repoanalyzer.reporeader.author.AuthorProvider;
+import org.repoanalyzer.reporeader.commit.Commit;
 
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Future;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class GitRepoReaderTest {
 //TODO: Try to mock it, but it may be hard to test GitRepoReader:getCommits()
-//    @Test
-//    public void getCommitsTest() throws Exception {
-//    }
+    @Test
+    public void getCommitsTest() throws Exception {
+        //given
+        String testPath = getClass().getResource("/TestAuthors.json").getPath();
+        while(!testPath.endsWith("Repo-Analyzer"))
+            testPath = testPath.substring(0,testPath.lastIndexOf('/'));
+        testPath += "/.git";
+        AuthorProvider authorProvider = new AuthorProvider();
+        GitRepoReader gitRepoReader = new GitRepoReader(testPath, authorProvider);
+
+        //when
+        Future<List<Commit>> commits = gitRepoReader.getCommits();
+
+        //then
+        while(!commits.isDone());
+    }
 
     @Test
     public void getProgressTest() throws Exception {
